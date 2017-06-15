9bcd118e988f0ccb19fb3c4006d036af38d36c84
David-OConnor <david.alan.oconnor@gmail.com>
2017-06-15 12:31:44 -0400

    Restructured to be independent of date-fns.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 Date Types
 ==========
 
-Date Types provides separate, immutable date and time types for Javascript. Theese types are designed to be used with the date-fns library. They're thin wrappers for the built-in Date type, and retain applicable methods. They introduce new, simpler properties to access components, and use UTC wherever possible.
+Date Types provides separate, immutable date and time types for Javascript. They're thin wrappers for the built-in Date type, and retain applicable methods. They introduce new, simpler properties to access components, and use UTC wherever possible. Date Types provides functions that work on these types, focusing on using a clean syntax.
 
 Motivation
 ----------
-Javascript's two leading libraries for dates and times, the built-in Date, and MomentJS, only provide combined dates and times. Dates are represented in these libraries by creating a datetime with 0 for all time components; times are represented by creating a datetime with 0 for all date components. This feels imprecise, and prone to errors; there are many cases where you might represent a pure date: Asking for its hours property shouldn't return 0; the query doens't make sense. Nor does adding 5 minutes to it.
+Javascript's two leading libraries for dates and times, the built-in Date, and MomentJS, only provide combined dates and times. Dates are represented in these libraries by creating a datetime with 0 for all time components; times are represented by creating a datetime with 0 for all date components. This feels imprecise, and prone to errors; there are many cases where you might represent a pure date: Asking for its hours property shouldn't return 0; the query doens't make sense. Nor does adding 5 minutes to it. Initially, this project was planned to work along-side the popular DateFns library, but we're unable to make our new types pass its validation at this point. DateFns is included as a dependency, and is used internally in a few places.
 
 Types
 -----
@@ -41,27 +41,55 @@ Setup
 
 Examples
 --------
-
+Creating dates and times
     // March 3, 2016; retains the built-in Date's use of 0-based Month indexing.
     const date = new DateOnly(2016, 2, 3)
-
-    date.year // 2016
-    date.getFullYear() // 2016
-
-    date.hour  // undefined
-    date.getHours() // Uncaught TypeError: date.getHours is not a function
-
-    dateFns.addDays(date, 5) // ERROR?
 
     // 11:30 am UTC; no seconds
     const time = new Time(11, 30)
 
+    // March 3, 2016, 11:30 am UTC; a combination of the above date and time.
+    const datetime = dt.combine(date, time)
+
+    const today = dt.today() // A DateOnly object of the current day
+
+Create a time delta: A difference between two times or dates
+    const delta = new TimeDelta(1, 1000)  / One day, one second
+
+Applicable methods from the built-in Date type are included:
+    date.year // 2016
+    date.getFullYear() // 2016
+
+Methods that mutate the date/time, or use inapplicable methods are not:
     time.setSeconds(30)  // Uncaught TypeError: time.setSeconds is not a function
     time.hour = 4  // ERROR; Time (and DateOnly/DateTime) are immutable.
+    date.hour  // undefined
+    date.getHours() // Uncaught TypeError: date.getHours is not a function
 
-    dt.today() // A DateOnly object of the current day
+Add or subtract amounts of time
+    dt.add(date, 5, "days") // March 8, 2016
+    dt.add(time, -5, "hours")  // 06:35
 
-    dt.combine(date, time) // March 3, 2016, 11:30 am UTC
+Adding amounts to the type that are innapropriate raises exceptions
+    dt.add(date, 5, "hours") // Exception: "Can't add hours to a date."
+    dt.add(time, 2, "days") // Exception: "Can't add days to a time."
+
+Create a time delta by taking the difference between two date/time objects.
+    const delta = dt.difference(date, new DateOnly(2016, 2, 1)
+
+Add or subtract a delta from a date/time
+    dt.addDelta(date, delta)
+    dt.subDelta(date, delta)
+
+You can't subtract a delta that includes a time component from a DateOnly, and vice versa:
+    dt.addDelta(time, delta)  // Exception: "Can't add a delta that includes date components to a time."
+
+
+Convert a date or time to a string
+    dt.format(datetime, 'YYYY-MM-DD HH:mm')  // '2016-03-03 11:30'
+
+But only if the string token makes sense for the type you're using
+    date.format(datetime, 'YYYY-MM-DD HH:mm') // 'String format tokens that only apply to times are not allowed on dateOnlys.'
 
 
 https://github.com/David-OConnor/date-types
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-// todo avoid getter/setter syntax.
+import * as dateFns from 'date-fns'
 
-
-class DateTime {
+export class DateTime {
     // We don't extend Date; extending built-in classes is awkward and controversial in JS.
     // There's no multiple inherritance in ES6, so DRY between these classes.
     readonly year: number
@@ -53,7 +52,7 @@ class DateTime {
 }
 
 
-class Time {
+export class Time {
     readonly hour: number
     readonly minute: number
     readonly second: number
@@ -107,7 +106,7 @@ class Time {
 }
 
 
-class DateOnly {
+export class DateOnly {
     readonly year: number
     readonly month: number
     readonly day: number
@@ -142,9 +141,35 @@ class DateOnly {
 }
 
 
+export class TimeDelta {
+    readonly days: number
+    readonly milliseconds: number
+
+    constructor(days: number=0, hours: number=0, minutes: number=0,
+                seconds: number=0, milliseconds: number=0) {
+                    this.days = days
+                    this.milliseconds = milliseconds + seconds * 1000 + minutes * 60 * 1000 + 
+                    hours * 3600 * 1000
+                }   
+}
 
 
-function split(dt: DateTime | Date) {
+export function fromDate(date: Date, category: string) {
+    // Convert a built-in date to a datetime, date, or time object.
+    if (category == 'datetime') {
+        return new DateTime(date.getUTCFullYear(), date.getUTCMonth(), date.getDay(),
+            date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds(), date.getUTCMilliseconds())
+    }
+    if (category == 'date') {
+        return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getDay())
+    }
+    if (category == 'time') {
+        return new Time(date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds(), date.getUTCMilliseconds())
+    }
+}
+
+
+export function split(dt: DateTime | Date) {
     // Split a DateTime (JS Date) into components
     const date = new DateOnly(dt.getUTCFullYear(), dt.getUTCMonth(), dt.getUTCDate())
     const time = new Time(dt.getUTCHours(), dt.getUTCMinutes(), dt.getUTCSeconds(), dt.getUTCMilliseconds())
@@ -153,48 +178,175 @@ function split(dt: DateTime | Date) {
 }
 
 
-function combine(date: DateOnly, time: Time) {
+export function combine(date: DateOnly, time: Time) {
     // Combine a date and time into a datetime
     return new DateTime(date.year, date.month, date.day, time.hour, time.minute, time.second, time.millisecond)
 }
 
+export function format(dt: Date | DateTime | DateOnly | Time, formatStr: string) {
+    const timeOnlyTokens= ['H', 'k', 'a', 'A', 'm', 's', 'S', 'Z']
+    const dateOnlyTokens= ['Y', 'Q', 'M', 'D','Do', 'x', 'X', 'g', 'w', 'e', 'd', 'gg', 'GG', 'W', 'E']
 
-function today() {
+    if (dt instanceof DateOnly) {
+        for (let token of timeOnlyTokens) {
+            if (formatStr.indexOf(token) >= 0) {
+                throw 'String format tokens that only apply to times are not allowed on dateOnlys.'
+            }
+        }
+    }
+    else if (dt instanceof Time) {
+        for (let token of dateOnlyTokens) {
+            if (formatStr.indexOf(token) >= 0) {
+                throw 'String format tokens that only apply to dates are not allowed on Times.'
+            }
+        }
+    }
+    if (dt instanceof Date) {
+        return dateFns.format(dt, formatStr)
+    }
+    return dateFns.format((dt as any).wrapped, formatStr)
+}
+
+
+export function add(dt: Date | DateTime | DateOnly | Time, amount: number, category: string) {
+    // Add (or subtract) a fixed amount of time from a Date/Datetime/dateOnly/Time object.
+   
+    if (dt instanceof Date) {
+        switch (category) {
+            case 'days':
+                return dateFns.addDays(dt, amount)
+            case 'months':
+                return dateFns.addMonths(dt, amount)
+            case 'years':
+                return dateFns.addYears(dt, amount)
+            case 'weeks':
+                return dateFns.addWeeks(dt, amount)
+            case 'hours':
+                return dateFns.addHours(dt, amount)
+            case 'minutes':
+                return dateFns.addMinutes(dt, amount)
+            case 'seconds':
+                return dateFns.addSeconds(dt, amount)
+            case 'milliseconds':
+                return dateFns.addMilliseconds(dt, amount)
+        }
+    }
+     let resultDate: Date
+
+    if (dt instanceof DateTime) {
+        switch (category) {
+            case 'days':
+                resultDate = dateFns.addDays((dt as any).wrapped, amount)
+            case 'months':
+                resultDate = dateFns.addMonths((dt as any).wrapped, amount)
+            case 'years':
+                resultDate = dateFns.addYears((dt as any).wrapped, amount)
+            case 'weeks':
+                resultDate = dateFns.addWeeks((dt as any).wrapped, amount)
+            case 'hours':
+                resultDate = dateFns.addHours((dt as any).wrapped, amount)
+            case 'minutes':
+                resultDate = dateFns.addMinutes((dt as any).wrapped, amount)
+            case 'seconds':
+                resultDate = dateFns.addSeconds((dt as any).wrapped, amount)
+            case 'milliseconds':
+                resultDate = dateFns.addMilliseconds((dt as any).wrapped, amount)
+        }
+
+        return fromDate(resultDate, 'datetime')
+    }
+
+    else if (dt instanceof Time) {
+        switch (category) {
+            case 'days':
+                throw "Can't add days to a time."
+            case 'months':
+                throw "Can't add months to a time."
+            case 'years':
+                throw "Can't add years to a time."
+            case 'weeks':
+                throw "Can't add weeks to a time."
+            case 'hours':
+                resultDate = dateFns.addHours((dt as any).wrapped, amount)
+            case 'minutes':
+                resultDate = dateFns.addMinutes((dt as any).wrapped, amount)
+            case 'seconds':
+                resultDate = dateFns.addSeconds((dt as any).wrapped, amount)
+            case 'milliseconds':
+                resultDate = dateFns.addMilliseconds((dt as any).wrapped, amount)
+        }
+
+        return fromDate(resultDate, 'time')
+    }
+
+    else if (dt instanceof DateOnly) {
+        switch (category) {
+            case 'days':
+                resultDate = dateFns.addDays((dt as any).wrapped, amount)
+            case 'months':
+                resultDate = dateFns.addMonths((dt as any).wrapped, amount)
+            case 'years':
+                resultDate = dateFns.addYears((dt as any).wrapped, amount)
+            case 'weeks':
+                resultDate = dateFns.addWeeks((dt as any).wrapped, amount)
+            case 'hours':
+                throw "Can't add hours to a date."
+            case 'minutes':
+                throw "Can't add minutes to a date."
+            case 'seconds':
+                throw "Can't add seconds to a date."
+            case 'milliseconds':
+                throw "Can't add milliseconds to a date."
+        }
+
+        return fromDate(resultDate, 'date')
+    }
+}
+
+
+export function addDelta(dt: Date | DateTime | DateOnly | Time, delta: TimeDelta) {
+    // Add a time delta to a date/time.
+    if (dt instanceof DateOnly) {
+        if (delta.milliseconds > 0) {
+            throw "Can't add a delta that includes time components to a dateOnly."
+        }
+        return new DateOnly(dt.year, dt.month, dt.day + delta.days)
+    }
+
+    if (dt instanceof Time) {
+        if (delta.days > 0) {
+            throw "Can't add a delta that includes date components to a time."
+        }
+        return new Time(dt.hour, dt.minute, dt.second, dt.millisecond + delta.milliseconds)
+    }
+
+    else if (dt instanceof DateTime) {
+        return new DateTime(dt.year, dt.month, dt.day + delta.days, dt.hour, dt.minute, 
+            dt.second, dt.millisecond + delta.milliseconds)
+    }
+
+}
+
+export function subDelta(dt: Date | DateTime | DateOnly | Time, delta: TimeDelta) {
+    // Subtract a time delta from a date/time.
+    const negativeDelta = new TimeDelta(-delta.days, 0, 0, 0, -delta.milliseconds)
+    return addDelta(dt, negativeDelta)
+}
+
+
+export function today() {
     // Return the current date.
     return split(new Date())[0]
 }
 
-export {DateTime, Time, DateOnly, today}
 
-import * as dateFns from 'date-fns'
+
 
 // let dt = new DateOnly(1999, 9, 9)
 
 let dt = new DateTime(1999, 9, 9, 1, 0, 0, 0)
-console.log(dt.year)
-
-let date = new DateOnly(2001, 4, 22)
-console.log(date.getDate())
-
-
 let time = new Time(11, 30)
-console.log(time.getHours())
-console.log(time.hour)
+let date = new DateOnly(2001, 4, 22)
 
-console.log(date.toString())
-console.log(time.toString())
-
-console.log(combine(date, time))
-console.log(combine(date, time).toString())
-// let dt = new DateTime()
-// console.log(dt.getHours(), "YEAH")
-
-
-console.log(date.hour)
-
-console.log(dateFns.addMilliseconds(date, 5))
-
-console.log(dateFns.format(date, 'YYYY-MMM-DD'))
-console.log(dateFns.format(time, 'HH:mm'))
-console.log(dateFns.format(dt, 'HH:mm'))
+console.log("HELLO")
 
